4001e51b65962e4474cc70ac34247cd000438eac
Sumit Gouthaman <sumitgt007@gmail.com>
2016-08-21 18:00:05 -0700

    Changing Android app icon + Beta label

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -11,7 +11,7 @@ using Xamarin.Forms;
 
 namespace MicrobitBLE.Droid
 {
-	[Activity(Label = "MicrobitBLE.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity(Label = "MicrobitBLE (Beta)", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
 		protected override void OnCreate(Bundle bundle)
